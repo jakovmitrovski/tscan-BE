c0693e7396b87032b992406ff45d74d54ae25a91
Jakov Mitrovski <jakovvmitrovski@yahoo.com>
2022-06-24 14:35:59 +0200

    add cloudfront link do cors config

--- src/main/java/com/example/squick/security/config/CorsConfig.java
+++ src/main/java/com/example/squick/security/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "http://tscan-fe.s3-website.eu-central-1.amazonaws.com")
+                .allowedOrigins("http://localhost:4200", "http://tscan-fe.s3-website.eu-central-1.amazonaws.com", "https://dwhas3jce10uu.cloudfront.net", "")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
